c6bf0899848ac3cc48b6da2fdaae5a41e4d76f4d
Anshcoder45 <93387770+Anshcoder45@users.noreply.github.com>
2022-12-16 21:13:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+console.log("Hi this is my first console screen");
+
+var firstname="Aadarsh";
+console.log(firstname);
+
+var lastname="kumar";
+console.log(lastname);
+
+var fullname= firstname.concat(lastname);
+console.log(fullname);
+
+var lengthfullname =fullname.length;
+console.log(lengthfullname);
+
+var name1="I am Aadarsh.K";
+console.log(name1);
+
+var replacename=name1.replace("I","Myself");
+console.log(replacename);
+
+var capitalname="Aadarsh";
+
+var number1=15;
+var number2=5;
+console.log(number1+number2);
+
+var number1=15;
+var number2=5;
+console.log(number1-number2);
+
+var number1=15;
+var number2=5;
+console.log(number1*number2);
+
+var number1=15;
+var number2=5;
+console.log(number1/number2);
